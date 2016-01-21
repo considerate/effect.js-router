ba5448940a3a3370b7f041617ce916c456e56c44
Viktor Erik Mikael Kronvall <viktor.kronvall@gmail.com>
2016-01-21 13:59:03 +0100

    Initial commit.

---
+++ index.js
@@ -0,0 +1,138 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.inputs = exports.Actions = undefined;
+
+var _effectjs = require('effectjs');
+
+function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj; }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var Actions = exports.Actions = (0, _effectjs.Types)('gotoPage', 'urlChanged', 'pageAction');
+
+var init = function init(router) {
+    return function (startpage) {
+        for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+            args[_key - 1] = arguments[_key];
+        }
+
+        var pages = router.pages;
+
+        var page = pages[startpage].component;
+        var result = page.init.apply(page, args);
+        return result.then(function (pageState, pageEffect) {
+            return (0, _effectjs.Result)({
+                page: page,
+                pageState: pageState,
+                pages: pages
+            }, pageEffect.map(_effectjs.Action.wrap(Actions.pageAction, { page: page })));
+        });
+    };
+};
+
+var update = function update(router) {
+    return function (state, action) {
+        var type = action.type;
+        var data = action.data;
+
+        if (type === Actions.gotoPage) {
+            var _ret = (function () {
+                var pagename = data.page;
+                var _data$args = data.args;
+                var args = _data$args === undefined ? [] : _data$args;
+                var pages = state.pages;
+
+                var page = pages[pagename].component;
+                return {
+                    v: page.init.apply(page, _toConsumableArray(args)).then(function (pageState, pageEffect) {
+                        return (0, _effectjs.Result)({
+                            page: page,
+                            pageState: pageState,
+                            pages: pages
+                        }, pageEffect.map(_effectjs.Action.wrap(Actions.pageAction, { page: page })));
+                    })
+                };
+            })();
+
+            if ((typeof _ret === 'undefined' ? 'undefined' : _typeof(_ret)) === "object") return _ret.v;
+        } else if (type === Actions.urlChanged) {} else if (type === Actions.pageAction) {
+            var _ret2 = (function () {
+                var pageState = state.pageState;
+                var page = state.page;
+                var pages = state.pages;
+                var actionPage = data.page;
+
+                if (page !== actionPage) {
+                    return {
+                        v: (0, _effectjs.Result)(state)
+                    }; //Do not handle actions for pages not shown
+                }
+                var pageAction = _effectjs.Action.unwrap(action);
+                return {
+                    v: page.update(pageState, pageAction).then(function (nextPageState, nextPageEffect) {
+                        return (0, _effectjs.Result)({
+                            page: page,
+                            pageState: nextPageState,
+                            pages: pages
+                        }, nextPageEffect.map(_effectjs.Action.wrap(Actions.pageAction, { page: page })));
+                    })
+                };
+            })();
+
+            if ((typeof _ret2 === 'undefined' ? 'undefined' : _typeof(_ret2)) === "object") return _ret2.v;
+        }
+    };
+};
+
+var view = function view(router) {
+    return function (state, next) {
+        return state.page.view(state.pageState, _effectjs.Action.to(next, Actions.pageAction, { page: state.page }));
+    };
+};
+
+var Router = function Router(pages, elsePage) {
+    return {
+        pages: pages,
+        elsePage: elsePage,
+        when: function when(url, config) {
+            var pages = this.pages;
+            var elsePage = this.elsePage;
+
+            if (elsePage) {
+                throw new Error('Cannot chain whens after else');
+            }
+            pages[url] = config;
+            return Router(pages, elsePage);
+        },
+        else: function _else(config) {
+            var pages = this.pages;
+
+            return Router(pages, config);
+        },
+
+        get init() {
+            return init(this);
+        },
+        get update() {
+            return update(this);
+        },
+        get view() {
+            return view(this);
+        }
+    };
+};
+
+exports.default = function () {
+    return Router({}, undefined);
+};
+
+var inputs = exports.inputs = new Observable(function (observer) {
+    if (window && 'onhashchange' in window) {
+        winow.onhashchange = function () {
+            observer.next((0, _effectjs.Action)(Actions.urlChanged, window.location.hash.substring(1)));
+        };
+    };
+});
---
+++ main.js
@@ -0,0 +1,109 @@
+import {Action,Result,Types} from 'effectjs';
+
+export const Actions = Types('gotoPage', 'urlChanged', 'pageAction');
+
+const init = (router) => (startpage, ...args) => {
+    const {pages} = router;
+    const page = pages[startpage].component;
+    const result = page.init(...args);
+    return result.then((pageState, pageEffect) => {
+        return Result(
+            {
+                page: page,
+                pageState: pageState,
+                pages: pages,
+            },
+            pageEffect.map(Action.wrap(Actions.pageAction, {page: page}))
+        );
+    });
+};
+
+const update = (router) => (state, action) => {
+    const {type, data} = action;
+    if(type === Actions.gotoPage) {
+        const {page: pagename, args: args = []} = data;
+        const {pages} = state;
+        const page = pages[pagename].component;
+        return page.init(...args)
+        .then((pageState, pageEffect) => {
+            return Result(
+                {
+                    page: page,
+                    pageState: pageState,
+                    pages,
+                },
+                pageEffect.map(Action.wrap(Actions.pageAction, {page: page}))
+            );
+        });
+    } else if(type === Actions.urlChanged) {
+
+    } else if(type === Actions.pageAction) {
+        const {pageState, page, pages} = state;
+        const {page: actionPage} = data;
+        if(page !== actionPage) {
+            return Result(state); //Do not handle actions for pages not shown
+        }
+        const pageAction = Action.unwrap(action);
+        return page.update(pageState, pageAction)
+        .then((nextPageState, nextPageEffect) => {
+            return Result(
+                {
+                    page: page,
+                    pageState: nextPageState,
+                    pages,
+                },
+                nextPageEffect.map(Action.wrap(Actions.pageAction, {page: page}))
+            );
+        });
+    }
+};
+
+const view = (router) => (state, next) => {
+    return state.page.view(
+        state.pageState,
+        Action.to(next, Actions.pageAction, {page: state.page})
+    );
+};
+
+
+const Router = (pages, elsePage) => {
+    return {
+        pages,
+        elsePage,
+        when(url, config) {
+            const {pages, elsePage} = this;
+            if(elsePage) {
+                throw new Error('Cannot chain whens after else');
+            }
+            pages[url] = config;
+            return Router(pages, elsePage);
+        },
+        else(config) {
+            const {pages} = this;
+            return Router(pages, config);
+        },
+        get init() {
+            return init(this);
+        },
+        get update() {
+            return update(this);
+        },
+        get view() {
+            return view(this);
+        }
+    };
+}
+
+
+export default () => {
+    return Router({},undefined);
+};
+
+export const inputs = new Observable(observer => {
+    if(window && 'onhashchange' in window) {
+        winow.onhashchange = () => {
+            observer.next(Action(Actions.urlChanged, window.location.hash.substring(1)));
+        };
+    };
+});
+
